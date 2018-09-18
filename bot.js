f5c9125479f7682356248613a2bf108786fcf685
Sebastián Ustinelli <ustinelli@hotmail.com>
2018-09-17 19:28:00 -0500

    DEPLOY

--- bot.js
+++ bot.js
@@ -1,13 +1,3 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-app.listen(server_port, server_ip_address, function () {
-
-    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
-
-});
-
 var ffmpeg = require('ffmpeg');
 const Discord = require('discord.js');
 const client = new Discord.Client();
